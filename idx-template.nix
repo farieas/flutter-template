bb95b34f1c7873ffa61b801881dfc6bbd5fc6c51
farhan <farhankdi@gmail.com>
2025-09-17 01:05:28 +0530

    Refactor Nix configuration and update Flutter template for improved environment setup and parameter options

--- idx-template.nix
+++ idx-template.nix
@@ -1,17 +1,18 @@
-# No user-configurable parameters
-{ pkgs, ... }: {
-  # Shell script that produces the final environment
-  bootstrap = ''
-    # Copy the folder containing the `idx-template` files to the final
-    # project folder for the new workspace. ${./.} inserts the directory
-    # of the checked-out Git folder containing this template.
-    cp -rf ${./.} "$out"
-
-    # Set some permissions
-    chmod -R +w "$out"
-
-    # Remove the template files themselves and any connection to the template's
-    # Git repository
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
-  '';
+{pkgs, template ? "app", platforms ? "web,android", ...}: {
+    packages = [
+        pkgs.curl
+        pkgs.gnutar
+        pkgs.xz
+        pkgs.git
+        pkgs.busybox
+        pkgs.flutter
+        pkgs.dart
+    ];
+    bootstrap = ''
+        flutter create "$out" --template="${template}" --platforms="${platforms}"
+        mkdir "$out"/.idx
+        cp ${./dev.nix} "$out"/.idx/dev.nix
+        install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
+        chmod -R u+w "$out"
+    '';
 }
